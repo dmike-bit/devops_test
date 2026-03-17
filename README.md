b117946871edc4fc3a4a19d9b26460eb58481494
dmike-bit <d.mike.s@yandex.ru>
2026-03-17 14:28:41 +0300

    reload

--- README.md
+++ README.md
@@ -2,9 +2,10 @@ Effective Mobile DevOps Тестовое задание
 
 Как запустить проект
 
-1.Клонировать репозиторий
+Клонировать репозиторий
 
 git clone <https://github.com/dmike-bit/devops_test>
+cd devops_test
 docker-compose up -d
 curl http://localhost
 Должны увидеть: Hello from Effective Mobile!
--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,19 @@ import socketserver
 
 PORT = 8080
 
-Handler = http.server.SimpleHTTPRequestHandler
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello from Effective Mobile!")
+        else:
+            # Для других путей можно вернуть 404
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not Found")
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("Hello from Effective Mobile!", PORT)
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Server running on port {PORT}")
     httpd.serve_forever()
